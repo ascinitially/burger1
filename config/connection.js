8c6854586168d4dcd5b1092ddc497e790ddbf8fb
ascinitially <Blaise@Blaises-MacBook-Pro.local>
2019-05-14 23:59:46 -0400

    Heroku setup2

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,13 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
+// mysql://bb60f738bf0b6e:8ece0986@us-cdbr-iron-east-02.cleardb.net/heroku_3f2114fb02a15d5?reconnect=true
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 8889,
-  user: "root",
-  password: "root",
-  database: "burgers_db"
+  host: "us-cdbr-iron-east-02.cleardb.net",
+  // port: 8889,
+  user: "bb60f738bf0b6e",
+  password: "8ece0986",
+  database: "heroku_3f2114fb02a15d5"
 });
 
 // Make connection.
